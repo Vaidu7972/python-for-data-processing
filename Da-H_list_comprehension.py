9fca605e4a6a115b1d7bed097c2bdc30243e6ccf
Vaidu7972 <142891413+Vaidu7972@users.noreply.github.com>
2026-07-01 14:18:56 +0530

    list comprehension concise way of creating list  in python

---
+++ Da-H_list_comprehension.py
@@ -0,0 +1,10 @@
+"""
+list comprehension : concise (giving info clearly) way to create lists in python
+
+new_list = [expression for item in iterable if condition] """
+
+numbers = [1, 2, 3, 4, 5]
+squares = [x ** 2 for x in numbers]
+print(squares) # Output: [1, 4, 9, 16, 25] 
+
+
